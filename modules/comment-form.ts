05e3e4b4cb5f9bb1387d95de6916676ece0836a3
SnG1205 <sergey.dn2003@gmail.com>
2025-10-31 16:57:59 +0100

    Part 5.1 (sub-task 7): Added "Add coment" web component; documentation to be done; refactoring of styles in .css is needed

--- modules/comment-form.ts
+++ modules/comment-form.ts
@@ -1,37 +1,54 @@
-export const displayCommentForm = (): void => {
-  const form: HTMLFormElement | null = document.querySelector('.comment-form');
-  const list: HTMLUListElement | null =
-    document.querySelector('.comment-container');
-  const nameField: HTMLInputElement | null = document.querySelector('#name');
-  const commentField: HTMLInputElement | null =
-    document.querySelector('#comment');
-
-  form.onsubmit = (e) => {
-    e.preventDefault();
-
-    const listItem = document.createElement('li');
-    const namePara = document.createElement('p');
-    const commentPara = document.createElement('p');
-    const nameValue = nameField.value;
-    const commentValue = commentField.value;
-
-    namePara.textContent = nameValue;
-    commentPara.textContent = commentValue;
-
-    console.log(nameValue);
-    if (!isEmpty(nameValue) && !isEmpty(commentValue)) {
-      list.appendChild(listItem);
-      listItem.appendChild(namePara);
-      listItem.appendChild(commentPara);
-    } else {
-      console.log('Fields must not be empty');
-    }
-
-    nameField.value = '';
-    commentField.value = '';
-  };
-};
+export const displayCommentForm = (): void => {};
 
 function isEmpty(text: string): boolean {
   return text.replace(/\s/g, '').length === 0;
 }
+
+customElements.define(
+  'add-comment-section',
+  class extends HTMLElement {
+    constructor() {
+      super();
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      const template = document.getElementById(
+        'add-comment'
+      ) as HTMLTemplateElement;
+      const templateContent = template.content;
+      shadowRoot.appendChild(templateContent);
+
+      const form: HTMLFormElement | null =
+        shadowRoot.querySelector('.comment-form');
+      const list: HTMLUListElement | null =
+        shadowRoot.querySelector('.comment-container');
+      const nameField: HTMLInputElement | null =
+        shadowRoot.querySelector('#name-field');
+      const commentField: HTMLInputElement | null =
+        shadowRoot.querySelector('#comment-field');
+
+      form.onsubmit = (e) => {
+        e.preventDefault();
+
+        const listItem = document.createElement('li');
+        const namePara = document.createElement('p');
+        const commentPara = document.createElement('p');
+        const nameValue = nameField.value;
+        const commentValue = commentField.value;
+
+        namePara.textContent = nameValue;
+        commentPara.textContent = commentValue;
+
+        console.log(nameValue);
+        if (!isEmpty(nameValue) && !isEmpty(commentValue)) {
+          list.appendChild(listItem);
+          listItem.appendChild(namePara);
+          listItem.appendChild(commentPara);
+        } else {
+          console.log('Fields must not be empty');
+        }
+
+        nameField.value = '';
+        commentField.value = '';
+      };
+    }
+  }
+);
