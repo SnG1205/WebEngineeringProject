b71d59ec09217ef370dfadf57b62a6e89a5b915b
SnG1205 <sergey.dn2003@gmail.com>
2025-11-18 02:29:01 +0100

    Moving all components to App.svelte, still in progress

--- modules/main.ts
+++ modules/main.ts
@@ -1,34 +1,10 @@
 import { searchHighlighter } from './search';
 import { mount } from 'svelte';
-import Secondary from '../src/components/Secondary.svelte';
-import Table from '../src/components/Table.svelte';
-import AudioSection from '../src/components/AudioSection.svelte';
-import HabitatsSection from '../src/components/HabitatsSection.svelte';
-import MoreBears from '../src/components/MoreBears.svelte';
-import CommentsSection from '../src/components/CommentsSection.svelte';
-
-const init = async (): Promise<void> => {
-  searchHighlighter();
-};
+import App from '../src/App.svelte';
 
 document.addEventListener('DOMContentLoaded', () => {
-  void init();
-  mount(Secondary, {
-    target: document.getElementById('secondary') as Element,
-  });
-  mount(Table, {
-    target: document.getElementById('table') as Element,
-  });
-  mount(AudioSection, {
-    target: document.getElementById('audio-section') as Element,
-  });
-  mount(HabitatsSection, {
-    target: document.getElementById('habitats-section') as Element,
-  });
-  mount(MoreBears, {
-    target: document.getElementById('more-bears') as Element,
-  });
-  mount(CommentsSection, {
-    target: document.getElementById('comment-section') as Element,
+  searchHighlighter();
+  mount(App, {
+    target: document.getElementById('app') as Element,
   });
 });
---
+++ src/App.svelte
@@ -0,0 +1,140 @@
+<script>
+    import AudioSection from "./components/AudioSection.svelte";
+    import CommentsSection from "./components/CommentsSection.svelte";
+    import HabitatsSection from "./components/HabitatsSection.svelte";
+    import MoreBears from "./components/MoreBears.svelte";
+    import Navigation from "./components/Navigation.svelte";
+    import Secondary from "./components/Secondary.svelte";
+    import Table from "./components/Table.svelte";
+</script>
+
+<main>
+    <article>
+        <Table />
+        <HabitatsSection />
+        <AudioSection />
+
+        <aside>
+            <h3 tabindex="-1">About the author</h3>
+            <p>Evan Wild is an unemployed plumber from Doncaster...</p>
+        </aside>
+
+        <CommentsSection />
+        <MoreBears />
+    </article>
+
+    <Secondary />
+
+    <footer>
+        <p>©Copyright by nobody (brother of somebody and anybody, member of Everybody family). All rights are NOT reserved.</p>
+    </footer>
+</main>
+
+<style>
+    * {
+        font-family: Arial, sans-serif;
+    }
+    p, input, li {
+        font-family: 'Open Sans Condensed', sans-serif;
+        color: #2a2a2a;
+    }
+
+    p, li {
+        font-size: 1.6rem;
+        line-height: 1.5;
+    }
+
+    /* || header layout */
+
+    div[class="nav"], article, footer {
+        background-color: #8FFF8F;
+    }
+
+    article, footer {
+        padding: 10px 30px;
+    }
+
+    article {
+        margin-right: 10px;
+    }
+
+    div[class="nav"] {
+        height: 50px;
+        background-color: #8FFF8F;
+        display: flex;
+        padding: 1% 0;
+        margin-bottom: 10px;
+    }
+
+    div[class="nav"] ul {
+        padding: 0;
+        list-style-type: none;
+        flex: 2;
+        display: flex;
+    }
+
+    div[class="nav"] li {
+        display: inline;
+        text-align: center;
+        flex: 1;
+    }
+
+    div[class="nav"] a {
+        display: inline-block;
+        font-size: 2rem;
+        text-transform: uppercase;
+        text-decoration: none;
+        color: #242424;
+    }
+
+    div[class="nav"] .search {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        height: 100%;
+        padding: 0 2em;
+    }
+
+    div[class="header"] {
+        background-color: #444488;
+        color: #E8E8E8;
+    }
+
+    div[class="header"] H1 {
+        font-size: 4rem;
+        text-align: center;
+        color: white;
+        text-shadow: 2px 2px 10px black;
+        font-family: 'Sonsie One', cursive;
+    }
+
+    .search input {
+        font-size: 1.6rem;
+        height: 32px;
+    }
+
+    .search input[type="search"] {
+        flex: 3;
+    }
+
+    .search input[type="submit"] {
+        flex: 1;
+        margin-left: 1rem;
+        background: #333;
+        border: 0;
+        color: white;
+    }
+
+    main {
+        display: flex;
+    }
+
+    article {
+        flex: 5;
+    }
+
+    footer {
+        margin-top: 10px;
+    }
+    .highlight { background-color: yellow; color: black; }
+</style>
